7f8581b5b4936d778658bbe5b3481e6dc0fa3ae7
hac1686 <73030989+hac1686@users.noreply.github.com>
2020-10-31 14:06:23 -0400

    add model for Articles

--- djangonautic/articles/models.py
+++ djangonautic/articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. This is a db table
+class Article(models.Model):  #this class inherites from models.Model
+  title = models.CharField(max_length=100)
+  slug = md
+  body
+  date
+  #thumbnail
+  #author
+  #slug
