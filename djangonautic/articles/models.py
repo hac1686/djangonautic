6dc91808f07c1f75201702f2c3ab07d85d81398d
hac1686 <73030989+hac1686@users.noreply.github.com>
2020-10-31 14:13:34 -0400

    update models file

--- djangonautic/articles/models.py
+++ djangonautic/articles/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here. This is a db table
 class Article(models.Model):  #this class inherites from models.Model
   title = models.CharField(max_length=100)
-  slug = md
-  body
-  date
+  slug = models.SlugField()
+  body = models.TextField()
+  date = models.DateTimeField(auto_now_add=True)
   #thumbnail
   #author
   #slug
